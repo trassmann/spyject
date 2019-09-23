8c865f96a3e49549510cc1a4e6d5ed1580f8be2d
Tormen Rassmann <tormen.rassmann@eatfirst.com>
2019-09-23 14:06:18 +0200

    add function spy, improve loggin

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-const makeSpyject = (spyName = "defaultSpy") => {
+export const makeObjectSpy = (targetObj = {}, spyName = "defaultObjectSpy") => {
+  console.log(
+    `Creating object spy "${spyName}" for object: ${JSON.stringify(targetObj)}`
+  );
   const spyHandlers = {
     get: (obj, propName) => {
-      console.log(`Reading property ${propName} on spy ${spyName} in ${obj}.`);
+      console.log(
+        `Reading property "${propName.toString()}" on spy "${spyName}" in ${JSON.stringify(
+          obj
+        )}.`
+      );
       return obj[propName];
     },
     set: (obj, propName, value) => {
       console.log(
-        `Writing value ${value} to property ${propName} on spy ${spyName} in ${obj}.`
+        `Writing value ${JSON.stringify(
+          value
+        )} to property "${propName.toString()}" on spy "${spyName}" in ${JSON.stringify(
+          obj
+        )}.`
       );
       obj[propName] = value;
       return true;
     }
   };
-  return new Proxy({}, spyHandlers);
+  return new Proxy(targetObj, spyHandlers);
 };
 
-export default makeSpyject;
+export const makeFunctionSpy = (
+  targetFunc = () => {},
+  spyName = "defaultFunctionSpy"
+) => {
+  console.log(
+    `Creating function spy ${spyName} for function: ${JSON.stringify(
+      targetFunc
+    )}`
+  );
+  const spyHandlers = {
+    apply: function(target, thisArg, argumentsList) {
+      console.log(
+        `Called function spy "${spyName}" with arguments ${argumentsList} and context: ${JSON.stringify(
+          thisArg
+        )}.`
+      );
+
+      return target(...argumentsList);
+    }
+  };
+  return new Proxy(targetFunc, spyHandlers);
+};
